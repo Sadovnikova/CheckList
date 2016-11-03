4f8ba52ece8aa4d2203308e009734bd1782d4167
sibmask <mask4job@yandex.ru>
2016-11-03 19:18:45 +0300

    build project with gradle

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,15 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        log.trace("start");
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        log.trace("end");
     }
 
 
