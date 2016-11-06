227883b4bd7385c996afddf58fdcecef1b67a8ee
sibmask <mask4job@yandex.ru>
2016-11-06 14:37:02 +0300

    Main window

---
+++ src/main/java/ru/sibmask/MainApp.java
@@ -0,0 +1,40 @@
+package ru.sibmask;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class MainApp extends Application{
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Checklist application");
+        initRootLayout();
+    }
+
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getClassLoader().getResource("MainApp.fxml"));
+            rootLayout = loader.load();
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            log.error("MainApp error: ", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
